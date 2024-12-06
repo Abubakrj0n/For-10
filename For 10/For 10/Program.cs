69812a7c26bf95574dcab2bd79ab4f3a4d4fa014
Abubakrj0n <big500530@gmail.com>
2024-12-06 19:07:56 +0500

    Add files via upload

---
+++ For 10/For 10/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace For
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        { 
+            double N = Convert.ToInt32(Console.ReadLine());
+
+        double sum = 0.0;
+
+        for (int i = 1; i <= N; i++)
+        {
+           sum += 1.0 / i;
+        }
+        Console.WriteLine(sum);
+        }
+    }
+}
+
+
